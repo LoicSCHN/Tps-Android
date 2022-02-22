2d4d4720dd6eba1c957b0819a3fc9c6dce0c6aaa
LSchnuriger <loic.cham74@gmail.com>
2022-02-23 00:07:19 +0100

    Exo1
    Exo1

--- TP2/app/src/main/java/com/example/tp2/MainActivity.java
+++ TP2/app/src/main/java/com/example/tp2/MainActivity.java
@@ -3,42 +3,57 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-   private SensorManager sensorManager ;
 
-    List<Sensor> sensorsList;
+    public final static String MESSAGE_EXO = "message_exo";
+    Button exo1;
+    Button exo2;
+    Button exo3;
+    Button exo4;
+    Button exo5;
+    Button exo6;
+    Button exo7;
+    Button exos[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorsList = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        listSensor();
 
-    }
+        exo1 = findViewById(R.id.exo1);
+        exo2 = findViewById(R.id.exo2);
+        exo3 = findViewById(R.id.exo3);
+        exo4 = findViewById(R.id.exo4);
+        exo5 = findViewById(R.id.exo5);
+        exo6 = findViewById(R.id.exo6);
+        exo7 = findViewById(R.id.exo7);
+        exos = new Button[]{exo1, exo2 , exo3, exo4, exo5, exo6, exo7};
 
-    private void listSensor() {
-        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        StringBuffer sensorDesc = new StringBuffer();
-        for (Sensor sensor : sensors) {
-            sensorDesc.append("New sensor detected : \r\n");
-            sensorDesc.append("\tName: " + sensor.getName() + "\r\n");
-            sensorDesc.append("\tType: " + sensor.getType() + "\r\n");
-            sensorDesc.append("Version: " + sensor.getVersion() + "\r\n");
-            sensorDesc.append("Resolution (in the sensor unit): " + sensor.getResolution() + "\r\n");
-            sensorDesc.append("Power in mA used by this sensor while in use" + sensor.getPower() +"\r\n");
-            sensorDesc.append("Vendor: " + sensor.getVendor() + "\r\n");
-            sensorDesc.append("Maximum range of the sensor in the sensor's unit." +  sensor.getMaximumRange() + "\r\n");
-            sensorDesc.append("Minimum delay allowed between two events in microsecond " + " or zero if this sensor only returns a value when the data it's measuring changes " + sensor.getMinDelay() + "\r\n");
+        for (Button b : exos){
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i1 = new Intent(MainActivity.this, Exos.class);
+
+                    i1.putExtra(MESSAGE_EXO, b.getText().toString());
+
+                    startActivity(i1);
+                }
+            });
         }
-        Toast.makeText(this, sensorDesc.toString(), Toast.LENGTH_LONG).show();
+
     }
+
+
 }
